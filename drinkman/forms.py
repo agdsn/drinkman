ac87bc48c8ccd90bf9515c48f48306ed2a1a73f5
Marcel Beyer <marcel.beyer@it-maker.eu>
2019-12-01 16:23:31 +0100

    improve delivery form

--- drinkman/forms.py
+++ drinkman/forms.py
@@ -6,13 +6,18 @@ from drinkman.models import User, Location
 class DeliveryForm(forms.Form):
     def __init__(self, items, *args, **kwargs):
         super(DeliveryForm, self).__init__(*args, **kwargs)
-        self.fields['user'] = forms.ChoiceField(label="Who are you?", choices=map(lambda u: (u.id, u.username),
-                                                                                  User.objects.all().order_by(
-                                                                                      'username')))
+        self.fields['user'] = forms.ChoiceField(label="Who are you?",
+                                                widget=forms.Select(attrs={'class': 'form-control'}),
+                                                choices=map(lambda u: (u.id, u.username),
+                                                            User.objects.all().order_by(
+                                                                'username')))
         self.fields['location'] = forms.ChoiceField(label="Location",
+                                                    widget=forms.Select(attrs={'class': 'form-control'}),
                                                     choices=map(lambda l: (l.id, l.name), Location.objects.all()))
         for item in items:
             self.fields["item_{}".format(item.id)] = forms.IntegerField(label=item.name, min_value=0, initial=0,
+                                                                        widget=forms.NumberInput(
+                                                                            attrs={'class': 'form-control'}),
                                                                         required=False)
         self.fields['check'] = forms.BooleanField(label='Input is correct', initial=False)
 
@@ -21,7 +26,7 @@ class StockForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super(StockForm, self).__init__(*args, **kwargs)
         attributes = {'onchange': 'actionform.submit();',
-                        'class':  'form-control mb-2 mr-sm-2'}
+                      'class': 'form-control mb-2 mr-sm-2'}
         self.fields['location'] = forms.ChoiceField(label="Location",
                                                     choices=map(lambda l: (l.id, l.name), Location.objects.all()),
                                                     widget=forms.Select(attrs=attributes))
