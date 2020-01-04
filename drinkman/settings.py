14421dc53a3b7e9d98d49988d1e4a38e8b27dc22
Jakob Müller <j.mueller@outlook.de>
2020-01-04 23:06:24 +0100

    Postgres support

--- drinkman/settings.py
+++ drinkman/settings.py
@@ -15,18 +15,16 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a*a)%eod2gm+p97de_-6-%y!qj3d5lk%6i=vf(g!089@^-s18l'
+SECRET_KEY = os.environ("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0']
-
 
 # Application definition
 
@@ -71,17 +69,26 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'drinkman.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'OPTIONS': {
+            'options': '-c search_path=drinkman'
+        },
+        'NAME': os.environ("DB_NAME"),
+        'USER': os.environ("DB_USER"),
+        'PASSWORD': os.environ("DB_PW"),
+        'HOST': os.environ("DB_HOST"),
+        'PORT': os.environ("DB_PORT"),
+    },
+    'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -101,7 +108,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -114,7 +120,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
