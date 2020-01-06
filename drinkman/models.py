d600f9e7f7fc8fb61946009275ac73653999fc2d
Jakob Müller <j.mueller@outlook.de>
2020-01-06 11:09:51 +0100

    Add string methods to model

--- drinkman/models.py
+++ drinkman/models.py
@@ -10,6 +10,9 @@ class User(models.Model):
     image_url = models.TextField(null=True)
     email = models.EmailField(null=True)
 
+    def __str__(self):
+        return self.username
+
     def token(self):
         return self.username[0]
 
@@ -43,6 +46,9 @@ class Stock(models.Model):
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
     amount = models.IntegerField(default=0)
 
+    def __str__(self):
+        return "{} @{}".format(self.item.name, self.location.name)
+
 
 class Transaction(models.Model):
     id = models.AutoField(primary_key=True)
@@ -50,3 +56,6 @@ class Transaction(models.Model):
     message = models.TextField()
     date = models.DateTimeField(default=datetime.now)
     item = models.ForeignKey(Item, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return "{}.{}.{}".format(self.id, self.user.username, self.date)
