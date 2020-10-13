7ec991cf05cf0e6a05c4ada03f82d8adc897a425
Jakob Mueller <jakob.mueller@ibot3.de>
2020-10-13 16:26:42 +0200

    Fix static files

--- drinkman/settings.py
+++ drinkman/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' if os.getenv("DJANGO_DEV") else 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
