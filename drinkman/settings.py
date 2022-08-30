e35856f24a977027d882deb50bb2ae85e7a7dbcc
Jakob Mueller <jakob.mueller@ibot3.de>
2022-08-30 15:06:39 +0200

    Remove whitenoise fully

--- drinkman/settings.py
+++ drinkman/settings.py
@@ -51,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'drinkman.urls'
